cdbfb6c90dc678e3d7e1b4dc36fa6a5222eacfff
Mickaël Brasebin <mickael.brasebin@gmail.com>
2019-01-29 00:21:27 +0100

    Solution

--- test.py
+++ test.py
@@ -1,14 +1,21 @@
 from qgis.core import *
 
-# supply path to qgis install location
-QgsApplication.setPrefixPath("/usr", True)
+def run():
+	# supply path to qgis install location
+	QgsApplication.setPrefixPath("/usr", True)
 
-# create a reference to the QgsApplication, setting the
-# second argument to False disables the GUI
-qgs = QgsApplication([], False)
+	# create a reference to the QgsApplication, setting the
+	# second argument to False disables the GUI
+	qgs = QgsApplication([], False)
 
-# load providers
-qgs.initQgis()
+	# load providers
+	qgs.initQgis()
 
 
-qgs.exitQgis()
+	print("I ran")
+
+	qgs.exitQgis()
+
+run()
+
+
